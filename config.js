710fb52d0fdc23204511e25fe163a773804bcf73
adrianfox035 <adrian.fox035@gmail.com>
2026-08-16 16:20:45 -0300

    Modify Supabase configuration in config.js
    Updated anonKey and removed adminEmail from config.

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
 window.SUPABASE_CONFIG = {
   url: "https://hwhihebohjuwooeehivq.supabase.co",
-  anonKey: "sb_publishable_CvZndoci7xeL9WJyKWtAGg_EOeilYFeC",
-
-  // E-mail do usuário administrador criado em:
-  // Supabase > Authentication > Users
-  adminEmail: "adrian.fox035@gmail.com"
+  anonKey: "sb_publishable_CvZndoci7xeL9WJyKWtAGg_EOeilYFe"
 };
